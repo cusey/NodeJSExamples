9fa19c0ef3085e22374391ac8b7b37423e638196
John Cusey <jjcusey@gmail.com>
2019-11-10 18:00:12 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,25 +3,25 @@
 #Introduction:
 #This is simple program that shows Hello World message to the screen.
 clear
-echo '< Introduction >'
+echo '< 1. Introduction >'
 echo 'helloWorld.js ****'
 echo ' '
 node helloWorld.js
 echo ' '
 # Function
-echo '< Function >'
+echo '< 2. Function >'
 echo 'app_function.js ****'
 node app_function.js
 echo ' '
 # Import Function
-echo '< Import Function >'
+echo '< 3. Import Function >'
 echo 'helpers_function.js ****'
 echo 'app_helpers_function.js ****'
 node app_helpers_function.js
 echo ' '
 # Arrow Function
 # Assign function to varaible
-echo '< Arrow Function >'
+echo '< 4. Arrow Function >'
 echo 'helpers_arrow_function.js ****'
 echo 'app_helpers_arrow_function.js ****'
 node app_helpers_arrow_function.js
