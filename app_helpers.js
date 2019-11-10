0e9ca37028aa785ac10c759d7b8b133ad631fc0d
John Cusey <jjcusey@gmail.com>
2019-11-10 14:45:20 -0500

    Export Functions

--- app_helpers.js
+++ app_helpers.js
@@ -4,3 +4,4 @@ const helpers = require("./helpers");
 const total = helpers.sum(10,200)
 
 console.log("TOTAL:", total );
+
--- helpers.js
+++ helpers.js
@@ -2,6 +2,10 @@ function sum(a,b){
     return a +b;
 }
 
+console.log("MODULE.EXPORT (before): ", module.exports);
+
 module.exports = {
     sum
 }
+
+console.log("MODULE.EXPORT (after): ", module.exports);
