22b42acb95db4379efec64f582309c077a5ee3b8
John Cusey <jjcusey@gmail.com>
2019-12-08 20:50:24 -0500

    app19

--- section2/nodeapi/routes/port19Route.js
+++ section2/nodeapi/routes/port19Route.js
@@ -4,10 +4,11 @@ console.log( scriptName + '_____________________________________' );
 
 const express = require("express");
 const postController = require('../controllers/post19Controller');
+const postValidator = require('../validators/post19Validator');
 
 const router = express.Router();
 
 router.get('/', postController.getPosts);
-router.post('/create_post', postController.createPost);
+router.post('/create_post', postValidator.createPost, postController.createPost);
 
 module.exports = router;
